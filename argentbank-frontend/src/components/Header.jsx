3db7cdb6fbe02ef80434b2b96267e2f7c1a3965c
kev6711 <kevin.heckenmeyer@gmail.com>
2025-10-28 11:52:01 +0100

    Ajout des composants Header et Footer + Modifications de la syntaxe des class et label for pour React + Redimensionnement et compression des images + Ajout cursor:pointer sur tous les boutons de l'application

--- argentbank-frontend/src/components/Header.jsx
+++ argentbank-frontend/src/components/Header.jsx
@@ -1,3 +1,35 @@
-function Header() {}
+import { Link, useLocation } from "react-router-dom"
+import argentBankLogo from "../assets/img/compressed/argentBankLogo.webp"
+
+function Header() {
+    const pathname = useLocation().pathname
+    return (
+        <nav className="main-nav">
+            <Link to="/" className="main-nav-logo">
+                <img className="main-nav-logo-image" src={argentBankLogo} alt="Argent Bank Logo" />
+                <h1 className="sr-only">Argent Bank</h1>
+            </Link>
+            {pathname === "/User" ? (
+                <div>
+                    <Link to="/User" className="main-nav-item">
+                        <i className="fa fa-user-circle"></i>
+                        Tony
+                    </Link>
+                    <Link to="/" className="main-nav-item">
+                        <i className="fa fa-sign-out"></i>
+                        Sign Out
+                    </Link>
+                </div>
+            ) : (
+                <div>
+                    <Link to="/SignIn" className="main-nav-item">
+                        <i className="fa fa-user-circle"></i>
+                        Sign In
+                    </Link>
+                </div>
+            )}
+        </nav>
+    )
+}
 
 export default Header
--- argentbank-frontend/src/pages/SignIn.jsx
+++ argentbank-frontend/src/pages/SignIn.jsx
@@ -1,48 +1,32 @@
 import "../assets/css/main.css"
 import { Link } from "react-router-dom"
-import argentBankLogo from "../assets/img/argentBankLogo.png"
 
 function SignIn() {
     return (
         <div>
-            <nav class="main-nav">
-                <Link to="/" class="main-nav-logo">
-                    <img class="main-nav-logo-image" src={argentBankLogo} alt="Argent Bank Logo" />
-                    <h1 class="sr-only">Argent Bank</h1>
-                </Link>
-                <div>
-                    <Link to="/SignIn" class="main-nav-item">
-                        <i class="fa fa-user-circle"></i>
-                        Sign In
-                    </Link>
-                </div>
-            </nav>
-            <main class="main bg-dark">
-                <section class="sign-in-content">
-                    <i class="fa fa-user-circle sign-in-icon"></i>
+            <main className="main bg-dark">
+                <section className="sign-in-content">
+                    <i className="fa fa-user-circle sign-in-icon"></i>
                     <h1>Sign In</h1>
                     <form>
-                        <div class="input-wrapper">
-                            <label for="username">Username</label>
+                        <div className="input-wrapper">
+                            <label htmlFor="username">Username</label>
                             <input type="text" id="username" />
                         </div>
-                        <div class="input-wrapper">
-                            <label for="password">Password</label>
+                        <div className="input-wrapper">
+                            <label htmlFor="password">Password</label>
                             <input type="password" id="password" />
                         </div>
-                        <div class="input-remember">
+                        <div className="input-remember">
                             <input type="checkbox" id="remember-me" />
-                            <label for="remember-me">Remember me</label>
+                            <label htmlFor="remember-me">Remember me</label>
                         </div>
                         <Link to="/User">
-                            <button class="sign-in-button">Sign In</button>
+                            <button className="sign-in-button">Sign In</button>
                         </Link>
                     </form>
                 </section>
             </main>
-            <footer class="footer">
-                <p class="footer-text">Copyright 2020 Argent Bank</p>
-            </footer>
         </div>
     )
 }
