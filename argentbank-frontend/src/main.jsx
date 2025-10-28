d35da506264e33f36c47147d05767051594addb8
kev6711 <kevin.heckenmeyer@gmail.com>
2025-10-28 12:52:08 +0100

    Installation et configuration de Redux

--- argentbank-frontend/src/main.jsx
+++ argentbank-frontend/src/main.jsx
@@ -2,8 +2,19 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import AppRouter from "./components/Router/AppRouter.jsx"
 
+import { Provider } from "react-redux"
+import { configureStore } from "@reduxjs/toolkit"
+import rootReducer from "./reducers"
+
+const store = configureStore({
+    reducer: rootReducer,
+    devTools: true,
+})
+
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <AppRouter />
+        <Provider store={store}>
+            <AppRouter />
+        </Provider>
     </StrictMode>
 )
