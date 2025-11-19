caa44194896570e84513cc186e8f33647910ca11
kev6711 <kevin.heckenmeyer@gmail.com>
2025-11-19 12:08:18 +0100

    Ajout composant HomeFeatureItem pour la page d'accueil + Ajout composant UserAccount pour la page User

---
+++ argentbank-frontend/src/components/HomeFeatureItem.jsx
@@ -0,0 +1,11 @@
+function HomeFeatureItem({ image, alt, title, description }) {
+    return (
+        <div className="feature-item">
+            <img src={image} alt={alt} className="feature-icon" />
+            <h3 className="feature-item-title">{title}</h3>
+            <p>{description}</p>
+        </div>
+    )
+}
+
+export default HomeFeatureItem
--- argentbank-frontend/src/pages/User.jsx
+++ argentbank-frontend/src/pages/User.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux"
 import { useState } from "react"
 import "../assets/css/main.min.css"
 import EditUserForm from "../components/EditUserForm"
-import UserAccounts from "../components/UserAccounts"
+import UserAccount from "../components/UserAccounts"
 
 function User() {
     const firstName = useSelector((state) => state.userGetReducer.firstName)
@@ -26,7 +26,21 @@ function User() {
                         </button>
                     )}
                 </div>
-                <UserAccounts />
+                <div className="accounts">
+                    <h2 className="sr-only">Accounts</h2>
+                    <UserAccount
+                        title="Argent Bank Checking (x8349)"
+                        amount="$2,082.79"
+                        description="Available Balance"></UserAccount>
+                    <UserAccount
+                        title="Argent Bank Savings (x6712)"
+                        amount="$10,928.42"
+                        description="Available Balance"></UserAccount>
+                    <UserAccount
+                        title="Argent Bank Credit Card (x8349)"
+                        amount="$184.30"
+                        description="Current Balance"></UserAccount>
+                </div>
             </main>
         </div>
     )
